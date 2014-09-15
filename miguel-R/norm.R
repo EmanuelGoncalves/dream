dcf07372bc9b1cd119444626813ffbb51f398072
Miguel Rocha <miguelrocha@Miguels-MacBook-Air-2.local>
2014-09-15 13:49:32 +0100

    add norm

---
+++ miguel-R/norm.R
@@ -0,0 +1,5 @@
+norm.input.tr.mat.2 = scale(input.tr.mat.2)
+colnames(norm.input.tr.mat.2) = NULL
+norm.input.ts.mat.2 = scale(input.ts.mat.2)
+colnames(norm.input.ts.mat.2) = NULL
+norm.output.tr.mat.2 = scale(output.tr.mat.2)
